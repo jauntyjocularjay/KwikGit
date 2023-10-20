814a3417ad4683e08abb77e9e19cd2019fa7b9c4
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-10-20 01:09:02 +0000

    Releasing changes

--- gitBranches.sh
+++ gitBranches.sh
@@ -13,3 +13,12 @@ git checkout dev
 echo 'Branches created. If a branch already exists, the other were created.'
 
 echo 'You are now on the dev branch.'
+
+echo 'What is your GitHub email address? ' -sp email
+
+echo 'What is your GitHub name? ' -sp name
+
+git config --global user.email $email
+git config --global user.name $name
+
+echo 'Global email and name set.'
