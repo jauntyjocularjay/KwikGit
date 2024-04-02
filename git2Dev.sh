1a813e31d38be1216f1df63cd453806d701d7c36
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-04-01 20:55:33 -0700

    preparing to release beta changes to master

--- git2Dev.sh
+++ git2Dev.sh
@@ -4,12 +4,20 @@
 git checkout dev-jade
 git pull
 git add .
+<<<<<<< HEAD
 git commit -a -m "Releasing changes to dev"
+=======
+git commit -a -m "preparing to release changes from dev-jade to dev"
+>>>>>>> c01856235659dc4b927d12b70a8b9e35d3b83fbd
 git push
 
 git checkout dev
 git pull
+<<<<<<< HEAD
 git merge dev-jade -m "Releasing changes to dev"
+=======
+git merge dev-jade -m "Releasing changes from dev-jade to dev"
+>>>>>>> c01856235659dc4b927d12b70a8b9e35d3b83fbd
 git push
 
 git checkout dev-jade
